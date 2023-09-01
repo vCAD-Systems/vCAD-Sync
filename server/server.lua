aa23bd0f08b5e68cd84ed65518ab4eaa3250e415
Ffrankys <FfrankysZockt@gmail.com>
2023-09-01 15:40:43 +0200

    check if rs is nil

--- server/server.lua
+++ server/server.lua
@@ -19,7 +19,7 @@ AddEventHandler('onResourceStart', function(resourceName)
         if Config.CharSync.Multichar and Config.CharSync.Activated then
             if Config.CharSync.Id_Spalte == nil then
                 local rs = MySQL.Sync.fetchAll("SELECT * FROM users", {})
-                if rs[1].id == nil then
+                if rs ~= nil and rs[1] ~= nil and rs[1].id == nil then
                     MySQL.Sync.execute("ALTER TABLE `users` ADD `id` BIGINT NOT NULL AUTO_INCREMENT FIRST, ADD UNIQUE (`id`)")
                 end
             end
@@ -27,7 +27,7 @@ AddEventHandler('onResourceStart', function(resourceName)
 
         if Config.Vehicle.Activated then
             local rs = MySQL.Sync.fetchAll("SELECT * FROM owned_vehicles", {})
-            if rs[1].id == nil then
+            if rs ~= nil and rs[1] ~= nil and rs[1].id == nil then
                 MySQL.Sync.execute("ALTER TABLE `owned_vehicles` ADD `id` BIGINT NOT NULL AUTO_INCREMENT FIRST, ADD UNIQUE (`id`)")
             end
         end
